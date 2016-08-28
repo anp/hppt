a57089a4e5d381648e0ac93aac36058d3d97c981
Adam Perry <adam.n.perry@gmail.com>
2016-08-28 00:07:24 -0700

    Fixing extra whitespace.

--- cgi-bin/addition.py
+++ cgi-bin/addition.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     print('Content-Type:text/html') #HTML is following
     print()                         #Leave a blank line
     print('<h1>Addition Results</h1>')
-    
+
     try:
         num1=int(input_data["num1"].value)
         num2=int(input_data["num2"].value)
